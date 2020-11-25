72deb7fe8492b558900ccc55b813b1ed6a774fa4
Evan Karopoulos <evan.karopoulos@team.telstra.com>
2020-11-26 01:38:45 +1100

    added init script

--- scripts/bin/bootstrap.bash
+++ scripts/bin/bootstrap.bash
@@ -5,7 +5,7 @@ set -u
 set -o pipefail
 
 __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-templates_folder="${__dir}/templates"
+templates_folder="${__dir}/../templates"
 template="typescript"
 init_path="services"
 service_name=""
@@ -18,8 +18,7 @@ get_input() {
 
 copy_template() {
     echo "copying template '$template' to '$init_path/$service_name'..."
-    echo "mkdir -p ${__dir}/$init_path/$service_name && cp -a $templates_folder/$template ${__dir}/$init_path/$service_name"
-    mkdir -p ${__dir}/$init_path/$service_name && cp -a $templates_folder/$template ${__dir}/$init_path/$service_name
+    mkdir -p ${__dir}/../../$init_path/$service_name && cp -a $templates_folder/$template ${__dir}/../../$init_path/$service_name
 }
 
 get_input
