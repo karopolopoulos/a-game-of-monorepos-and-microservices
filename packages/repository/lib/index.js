8faa3cfddab2aabbf6391861a472e0dfe2f4a006
Evan Karopoulos <evan.karopoulos@team.telstra.com>
2020-11-26 12:03:19 +1100

    removed a package

--- packages/repository/lib/index.js
+++
@@ -1,8 +0,0 @@
-const main = () => {
-  const message = 'Hello world!';
-
-  console.log(message);
-  return message;
-};
-
-module.exports = { main };
--- packages/repository/test/index.test.js
+++
@@ -1,7 +0,0 @@
-const { main } = require('../lib/index');
-
-describe('Service Test', () => {
-  test('main function returns message', () => {
-    expect(main()).toEqual('Hello world!');
-  });
-});
